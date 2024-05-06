8224596285f4506a19752f270df237cf700235a4
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2024-05-06 17:06:59 +0530

    handled exceptions

--- src/main/java/cyborg/springboot/employee_rest_api/rest/EmployeeRESTController.java
+++ src/main/java/cyborg/springboot/employee_rest_api/rest/EmployeeRESTController.java
@@ -4,11 +4,10 @@ import cyborg.springboot.employee_rest_api.DAO.EmployeeDAO;
 import cyborg.springboot.employee_rest_api.entity.Employee;
 import cyborg.springboot.employee_rest_api.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,8 +35,43 @@ public class EmployeeRESTController {
         Employee theEmployee = employeeService.getEmployess(employeeId);
 
         if (theEmployee == null) {
-            throw new RuntimeException("Employee not found");
+            throw new EmployeeNotFoundException("Employee id not found - " + employeeId);
         }
         return theEmployee;
     }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeErrorResponse> handleException(EmployeeNotFoundException exp) {
+
+        // create a StudentErrorResponse
+
+        EmployeeErrorResponse response = new EmployeeErrorResponse();
+
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setMessage(exp.getMessage());
+        response.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+
+    }
+
+    // add another exception handler.. to catch any exception(catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeErrorResponse> handleException(Exception e) {
+
+        // create a StudentErrorResponse
+
+        EmployeeErrorResponse response = new EmployeeErrorResponse();
+
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setMessage(e.getMessage());
+        response.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
 }
